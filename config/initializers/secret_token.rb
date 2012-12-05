b7fe7bbbf2f8e7da209924d618301a3d290d9f3d
Shirshendu Chakrabarti <shirshen12@gmail.com>
2012-12-05 15:01:27 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GtSandwiches::Application.config.secret_token = 'ccd6d5fd0dc43c72572473c726cf646981b99e3b3cbcdf910942f5a41d4aec11b28a273f35e33845f6c4b0220e37c1d10272b4847b6bd73fb318c4c2a20355cd'
